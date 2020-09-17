1779b868dacbbfeeb8d9682118fcacac67947072
himansingh241 <himansingh24@gmail.com>
2020-09-17 15:19:31 +0530

    Handlers added

--- product-api/main.go
+++ product-api/main.go
@@ -4,11 +4,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/the-codingguy/microservice-go/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello()
 
-	http.ListenAndServe(":9090", nil)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodBye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	http.ListenAndServe(":9090", sm)
 }
